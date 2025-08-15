805c37ab72092152822eb8976694874a43d89624
joker876 <joker876x@gmail.com>
2025-08-15 08:40:39 +0200

    Fix issue #127 Targets are incorrectly marked as needing translation when the translation ends with an x tag (#128)
    * fix target being incorrenctly marked as needing translation when ending with x tag
    
    * also bug-proof two further cases

--- src/features/trans-check.ts
+++ src/features/trans-check.ts
@@ -23,22 +23,22 @@
  */
 
 import {
-    commands,
-    DecorationRenderOptions,
-    ExtensionContext,
-    Range,
-    Selection,
-    TextEditor,
-    TextEditorDecorationType,
-    TextEditorRevealType,
-    window,
-    workspace,
-    Uri,
-    WorkspaceConfiguration,
-    WorkspaceFolder
+  commands,
+  DecorationRenderOptions,
+  ExtensionContext,
+  Range,
+  Selection,
+  TextEditor,
+  TextEditorDecorationType,
+  TextEditorRevealType,
+  Uri,
+  window,
+  workspace,
+  WorkspaceConfiguration,
+  WorkspaceFolder
 } from 'vscode';
-import { XlfDocument } from './tools/xlf/xlf-document';
 import { FilesHelper, WorkspaceHelper, XmlNode } from './tools';
+import { XlfDocument } from './tools/xlf/xlf-document';
 import { translationState } from './tools/xlf/xlf-translationState';
 import open = require('open');
 
@@ -243,13 +243,14 @@ export class XliffTranslationChecker {
             'missingTranslation'
         ];
         if (missingTranslationKeyword === '%EMPTY%') {
-            missingTranslationKeyword = '<target[^>]*( state="needs-translation")?[^>]*/>|<target.*( state="needs-translation")?.*></target>';
+            missingTranslationKeyword =
+              '<target[^>]*( state="needs-translation")?[^>]*/>|<target[^>]*( state="needs-translation")?[^>]*></target>';
         }
         else if (decorationTargetTextOnly) {
-            missingTranslationKeyword = `(?<=<target.*( state="needs-translation")?.*>)${missingTranslationKeyword}(?=</target>)`;
+            missingTranslationKeyword = `(?<=<target[^>]*( state="needs-translation")?[^>]*>)${missingTranslationKeyword}(?=</target>)`;
         }
         else {
-            missingTranslationKeyword = `<target.*( state="needs-translation")?.*>${missingTranslationKeyword}</target>`;
+            missingTranslationKeyword = `<target[^>]*( state="needs-translation")?[^>]*>${missingTranslationKeyword}</target>`;
         }
         return missingTranslationKeyword;
     }
